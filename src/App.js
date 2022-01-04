b65da3c2c11dd8d414ebd85b737750edced7b04b
Anusha Dasari <anusha.dasari@tricecommunity.com>
2022-01-04 17:51:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Aboutpage from "./Aboutpage";
+import Dashboard from "./Dashboard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/Aboutpage" exact component={Aboutpage} />
+        <Route path="/Dashboard" exact component={Dashboard} />
+     </Routes> 
+     </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
